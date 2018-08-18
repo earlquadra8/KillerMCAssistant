8dc9920ee3ea2da0526a77f5e3d5cc72a09f164e
Earl-NB\Earl <earl.cy.li@gmail.com>
2018-08-18 19:56:33 +0800

    Created SkillBase

--- KillerMCAssistant/Assets/_Scripts/CharacterBase.cs
+++ KillerMCAssistant/Assets/_Scripts/CharacterBase.cs
@@ -8,7 +8,6 @@ public abstract class CharacterBase : MonoBehaviour
     public int characterId;
 
     #region Unity Mono   
-
     void Start ()
     {
         		
---
+++ KillerMCAssistant/Assets/_Scripts/SkillBase.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SkillBase : MonoBehaviour {
+
+    public string skillName;
+    public int skillId;
+    public bool isAnyTime;
+
+    #region Unity Mono
+    void Start ()
+    {
+		
+	}
+
+	void Update ()
+    {
+		
+	}
+    #endregion
+
+}

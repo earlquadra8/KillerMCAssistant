e3fcaec64082523289d2b5b8e018c1c8da7d27ce
Earl-NB\Earl <earl.cy.li@gmail.com>
2018-08-18 19:50:11 +0800

    Created CharacterBase

---
+++ KillerMCAssistant/Assets/_Scripts/CharacterBase.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CharacterBase : MonoBehaviour
+{
+    public string characterName;
+    public int characterId;
+
+    #region Unity Mono   
+
+    void Start ()
+    {
+        		
+	}
+	
+	void Update ()
+    {
+		
+	}
+    #endregion
+
+    public abstract void Skill();
+}
